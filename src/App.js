6fe8b8ae7920148a425c18bfd2e371614ba74b67
Mo Hampton <monique.j.hampton@gmail.com>
2020-08-06 23:23:30 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MemberList from './MemberList';
+
+function App() {
+  return (
+    <div>
+      <h3>Members</h3>
+      <MemberList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Member.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Member(props) {
+return (<div><span>{props.name}</span></div>);
+}
